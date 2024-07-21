de183cf465c16590db6cf5047eead42372e4f1ac
abdul khaliq <abdallamakhtal@gmail.com>
2024-07-21 16:53:57 +0300

    solution for the lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 function addingEventListener() {
-}
+    const input = document.getElementById('button');
+  
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+  
+    input.addEventListener('click', clickAlert);
+  }
+  
+  // Calling the addingEventListener function to ensure the listener is set up
+  addingEventListener();
+  
+  // Exporting the function if needed (depending on your test setup)
+  // module.exports = addingEventListener;
+  
